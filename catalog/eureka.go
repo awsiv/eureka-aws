14e45e8b3d076dd7dc88ca7347995ed5c2b2d2cb
Bishwa Shrestha <bishwa.shrestha@tidal.com>
2020-04-10 14:50:24 +0200

    eureka tests

--- catalog/eureka.go
+++ catalog/eureka.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 
-	e "github.com/ArthurHlt/go-eureka-client/eureka"
+	_e "github.com/ArthurHlt/go-eureka-client/eureka"
 	"github.com/hashicorp/go-hclog"
 )
 
@@ -21,7 +21,7 @@ const (
 // instance info:
 //  https://github.com/ArthurHlt/go-eureka-client/blob/3b8dfe04ec6ca280d50f96356f765edb845a00e4/eureka/requests.go#L45-L67
 type eureka struct {
-	client       *e.Client
+	client       *_e.Client
 	log          hclog.Logger
 	eurekaPrefix string
 	awsPrefix    string
@@ -142,39 +142,37 @@ func (e *eureka) sync(aws *aws, stop, stopped chan struct{}) {
 	}
 }
 
-func (e *eureka) transformNodes(cnodes []e.InstanceInfo) map[string]map[int]node {
+func (e *eureka) transformNodes(cnodes []_e.InstanceInfo) map[string]map[int]node {
 	nodes := map[string]map[int]node{}
 	attributes := make(map[string]string)
 
 	for _, n := range cnodes {
-		/*
-			privateip := n.DataCenterInfo.Metadata.LocalIpv4
+		privateip := n.DataCenterInfo.Metadata.LocalIpv4
 
-			if nodes[privateip] == nil {
-				nodes[privateip] = map[int]node{}
-			}
+		if nodes[privateip] == nil {
+			nodes[privateip] = map[int]node{}
+		}
 
-			ports := nodes[privateip]
+		ports := nodes[privateip]
 
-			attributes["public-ipv4"] = n.DataCenterInfo.Metadata.PublicIpv4
-			attributes["local-ipv4"] = n.DataCenterInfo.Metadata.LocalIpv4
-			attributes["public-hostname"] = n.DataCenterInfo.Metadata.PublicHostname
-			attributes["local-hostname"] = n.DataCenterInfo.Metadata.LocalHostname
-			attributes["availability-zone"] = n.DataCenterInfo.Metadata.AvailabilityZone
-			attributes["homePageUrl"] = n.HomePageUrl
-			attributes["statusPageUrl"] = n.StatusPageUrl
-			attributes["healthCheckUrl"] = n.HealthCheckUrl
+		attributes["public-ipv4"] = n.DataCenterInfo.Metadata.PublicIpv4
+		attributes["local-ipv4"] = n.DataCenterInfo.Metadata.LocalIpv4
+		attributes["public-hostname"] = n.DataCenterInfo.Metadata.PublicHostname
+		attributes["local-hostname"] = n.DataCenterInfo.Metadata.LocalHostname
+		attributes["availability-zone"] = n.DataCenterInfo.Metadata.AvailabilityZone
+		attributes["homePageUrl"] = n.HomePageUrl
+		attributes["statusPageUrl"] = n.StatusPageUrl
+		attributes["healthCheckUrl"] = n.HealthCheckUrl
 
-			ports[n.Port.Port] = node{port: n.Port.Port, host: attributes["local-ipv4"], eurekaID: n.App, awsID: n.App, attributes: attributes, instanceID: n.DataCenterInfo.Metadata.InstanceId}
-			nodes[privateip] = ports
-			e.log.Debug("transformNodes()", "port", n.Port.Port, "ipAddr", n.IpAddr, "attributes", attributes, "instanceId", n.DataCenterInfo.Metadata.InstanceId)
-		*/
+		ports[n.Port.Port] = node{port: n.Port.Port, host: attributes["local-ipv4"], eurekaID: n.App, awsID: n.App, attributes: attributes, instanceID: n.DataCenterInfo.Metadata.InstanceId}
+		nodes[privateip] = ports
+		//e.log.Debug("transformNodes()", "port", n.Port.Port, "ipAddr", n.IpAddr, "attributes", attributes, "instanceId", n.DataCenterInfo.Metadata.InstanceId)
 	}
 	return nodes
 }
 
 // read nodes from eureka
-func (e *eureka) fetchNodes(service string) ([]e.InstanceInfo, error) {
+func (e *eureka) fetchNodes(service string) ([]_e.InstanceInfo, error) {
 	app, err := e.client.GetApplication(service)
 	if err != nil {
 		return nil, fmt.Errorf("error querying Service nodes, will retry: %s", err)
@@ -182,12 +180,12 @@ func (e *eureka) fetchNodes(service string) ([]e.InstanceInfo, error) {
 	return app.Instances, err
 }
 
-func (e *eureka) transformHealth(ehealths []e.InstanceInfo) map[string]health {
+func (e *eureka) transformHealth(ehealths []_e.InstanceInfo) map[string]health {
 	healths := map[string]health{}
 
 	for _, h := range ehealths {
 		instanceId := h.DataCenterInfo.Metadata.InstanceId
-		e.log.Info("transformHealth()", "instanceID", instanceId, "status", h.Status)
+		//e.log.Info("transformHealth()", "instanceID", instanceId, "status", h.Status)
 
 		switch h.Status {
 		case "UP":
@@ -215,7 +213,7 @@ func (c *eureka) fetchHealth(name string) (api.HealthChecks, error) {
 }
 */
 
-func (e *eureka) fetchServices() (*e.Applications, error) {
+func (e *eureka) fetchServices() (*_e.Applications, error) {
 	apps, err := e.client.GetApplications()
 	if err != nil {
 		return apps, err
@@ -235,14 +233,12 @@ func (e *eureka) fetch() error {
 	return nil
 }
 
-func (e *eureka) transformServices(apps *e.Applications) map[string]service {
+func (e *eureka) transformServices(apps *_e.Applications) map[string]service {
 	services := make(map[string]service, len(apps.Applications))
 	for _, v := range apps.Applications {
 		//TODO: bishwa
 		if v.Name == "CORNELIUS" || v.Name == "s1" || v.Name == "s2" || v.Name == "s3" {
-			//s := service{id: v.Name, name: v.Name, eurekaID: v.Name, fromEureka: true}
-			s := service{id: "test", name: "v.Name", eurekaID: "v.Name", fromEureka: true}
-
+			s := service{id: v.Name, name: v.Name, eurekaID: v.Name, fromEureka: true}
 			/*
 				if s.fromAWS {
 					s.name = strings.TrimPrefix(v.Name, e.awsPrefix)
@@ -250,8 +246,8 @@ func (e *eureka) transformServices(apps *e.Applications) map[string]service {
 			*/
 
 			//e.log.Info("transformServices()", "serviceName", v.Name)
-			//s.nodes = e.transformNodes(v.Instances)
-			//s.healths = e.transformHealth(v.Instances)
+			s.nodes = e.transformNodes(v.Instances)
+			s.healths = e.transformHealth(v.Instances)
 
 			services[s.name] = s
 		}
---
+++ catalog/eureka_test.go
@@ -0,0 +1,164 @@
+package catalog
+
+import (
+	"testing"
+
+	_e "github.com/ArthurHlt/go-eureka-client/eureka"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEurekaRekeyHealths(t *testing.T) {
+	type variant struct {
+		nodes    map[string]map[int]node
+		healths  map[string]health
+		expected map[string]health
+	}
+	variants := []variant{
+		{
+			nodes:    map[string]map[int]node{},
+			healths:  map[string]health{},
+			expected: map[string]health{},
+		},
+		{
+			nodes: map[string]map[int]node{
+				"1.1.1.1": {8000: {port: 8000, awsID: "X1"}},
+			},
+			healths: map[string]health{
+				"web_1.1.1.1_8000": up,
+			},
+			expected: map[string]health{
+				"X1": up,
+			},
+		},
+	}
+
+	for _, v := range variants {
+		e := eureka{
+			services: map[string]service{
+				"s1": {
+					nodes:   v.nodes,
+					healths: v.healths,
+				},
+			},
+		}
+		require.Equal(t, v.expected, e.rekeyHealths("s1", e.services["s1"].healths))
+	}
+}
+
+func TestEurekaTransformServices(t *testing.T) {
+	e := eureka{awsPrefix: "aws_"}
+
+	services := _e.Applications{
+		Applications: []_e.Application{
+			{
+				Name: "s1",
+				Instances: []_e.InstanceInfo{
+					{
+						App:            "s1",
+						HostName:       "s1-hostname",
+						HomePageUrl:    "s1-homepageUrl",
+						StatusPageUrl:  "s1-statuspageUrl",
+						HealthCheckUrl: "s1-healthcheckUrl",
+						IpAddr:         "127.0.0.1",
+						InstanceID:     "i-nstanceIDs1",
+						Status:         "UP",
+						Port: &_e.Port{
+							Port:    1,
+							Enabled: true,
+						},
+						DataCenterInfo: &_e.DataCenterInfo{
+							Metadata: &_e.DataCenterMetadata{
+								InstanceId:       "i-nstanceIDs1",
+								LocalIpv4:        "1.1.1.1",
+								LocalHostname:    "s1-private-hostname",
+								PublicHostname:   "s1-public-hostname",
+								PublicIpv4:       "9.9.9.9",
+								AvailabilityZone: "us-east-1e",
+							},
+						},
+						LeaseInfo:  &_e.LeaseInfo{},
+						Metadata:   &_e.MetaData{},
+						SecurePort: &_e.Port{},
+					},
+				},
+			},
+			{
+				Name: "s2",
+				Instances: []_e.InstanceInfo{
+					{
+						App:            "s2",
+						HostName:       "s2-hostname",
+						HomePageUrl:    "s2-homepageUrl",
+						StatusPageUrl:  "s2-statuspageUrl",
+						HealthCheckUrl: "s2-healthcheckUrl",
+						IpAddr:         "127.0.0.1",
+						InstanceID:     "i-nstanceID",
+						Status:         "OUT_OF_SERVICE",
+						Port: &_e.Port{
+							Port:    2,
+							Enabled: true,
+						},
+						DataCenterInfo: &_e.DataCenterInfo{
+							Metadata: &_e.DataCenterMetadata{
+								InstanceId:       "i-nstanceIDs2",
+								LocalIpv4:        "1.1.1.2",
+								LocalHostname:    "s2-private-hostname",
+								PublicHostname:   "s2-public-hostname",
+								PublicIpv4:       "9.9.9.9",
+								AvailabilityZone: "us-east-1e",
+							},
+						},
+						LeaseInfo:  &_e.LeaseInfo{},
+						Metadata:   &_e.MetaData{},
+						SecurePort: &_e.Port{},
+					},
+				},
+			},
+		},
+	}
+
+	attributes_s1 := map[string]string{
+		"local-ipv4":        "1.1.1.1",
+		"local-hostname":    "s1-private-hostname",
+		"public-hostname":   "s1-public-hostname",
+		"public-ipv4":       "9.9.9.9",
+		"availability-zone": "us-east-1e",
+		"homePageUrl":       "s1-homepageUrl",
+		"statusPageUrl":     "s1-statuspageUrl",
+		"healthCheckUrl":    "s1-healthcheckUrl",
+	}
+	attributes_s2 := map[string]string{
+		"local-ipv4":        "1.1.1.2",
+		"local-hostname":    "s2-private-hostname",
+		"public-hostname":   "s2-public-hostname",
+		"public-ipv4":       "9.9.9.9",
+		"availability-zone": "us-east-1e",
+		"homePageUrl":       "s2-homepageUrl",
+		"statusPageUrl":     "s2-statuspageUrl",
+		"healthCheckUrl":    "s2-healthcheckUrl",
+	}
+
+	nodes_s1 := map[string]map[int]node{
+		"1.1.1.1": {1: {port: 1, host: "1.1.1.1", awsID: "s1", eurekaID: "s1", instanceID: "i-nstanceIDs1", attributes: attributes_s1}},
+	}
+
+	nodes_s2 := map[string]map[int]node{
+		// structure =  ip : {port:{ ... }}
+		"1.1.1.2": {2: {port: 2, host: "1.1.1.2", awsID: "s2", eurekaID: "s2", instanceID: "i-nstanceIDs2", attributes: attributes_s2}},
+		//		"1.1.1.3": {3: {port: 3, host: "1.1.1.3", eurekaID: "s2", instanceID: "i-nstanceIDs2", attributes: attributes_s2}},
+	}
+
+	health_s1 := map[string]health{
+		"i-nstanceIDs1": healthy,
+	}
+	health_s2 := map[string]health{
+		"i-nstanceIDs2": unhealthy,
+		//	"s3": unknown,
+	}
+
+	expected := map[string]service{
+		"s1": {id: "s1", name: "s1", eurekaID: "s1", fromEureka: true, nodes: nodes_s1, healths: health_s1},
+		"s2": {id: "s2", name: "s2", eurekaID: "s2", fromEureka: true, nodes: nodes_s2, healths: health_s2},
+	}
+	require.Equal(t, expected, e.transformServices(&services))
+}
